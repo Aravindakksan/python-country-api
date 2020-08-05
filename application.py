b4523202f0260f74dbd1e9cdde2ada1157a25af1
ARAVINDAKKSAN VIJAYAKUMAR <36226810+Aravindakksan@users.noreply.github.com>
2020-08-05 14:18:26 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,26 +8,6 @@ import pyodbc
 app = Flask(__name__)
 #CORS(app)
 countries=[]
-# Connect to the database
-server = 'tcp:country.database.windows.net' 
-database = 'rest country api' 
-username = 'aravind' 
-password = 'Akksan766764#'
-connection = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM country")
-rows = cursor.fetchall()
-rows=list(rows)
-
-
-key=('name','code','capital','region','subregion','population','area','lat','lon','timezone','currency','flag')
-for i in range(len(rows)):
-    country={}
-    for j in range(len(rows[i])):
-        t=key[j]
-        country[t]=rows[i][j]
-    countries.append(country)
-
 
 @app.route("/name")
 def country_name():
@@ -94,54 +74,3 @@ def country_continent():
             c_continent.append(dic)
     return jsonify(c_continent)
 
-@app.route("/")
-def home():
-    name=[]
-    ccode=[]
-    capital=[]
-    region=[]
-    subregion=[]
-    population=[]
-    area=[]
-    latlong=[]
-    timezone=[]
-    currency=[]
-    flag=[]
-
-    with request.urlopen('https://restcountries.eu/rest/v2') as response:
-        if response.getcode() == 200:
-            source = response.read()
-            data = json.loads(source)
-        else:
-            print('An error occurred while attempting to retrieve data from the API.')
-        for i in range(0,len(data)):
-            name.append(data[i]['name'])
-            ctemp=str(data[i]['callingCodes'][0])
-            ccode.append(ctemp)
-            capital.append(data[i]['capital'])
-            region.append(data[i]['region'])
-            subregion.append(data[i]['subregion'])
-            population.append(data[i]['population'])
-            if(str(data[i]['area'])=='None'):
-                area.append(0)
-            else:
-                area.append(data[i]['area'])
-            if(data[i]['latlng']==[]):
-                latlong.append([0,0])
-            else:    
-                latlong.append(data[i]['latlng'])
-            ttemp=(str(data[i]['timezones'][0]))
-            timezone.append(ttemp)
-            currency.append(data[i]['currencies'][0]['name'])
-            flag.append(data[i]['flag'])
-
-    
-    # Create a new record
-    if(len(rows)<250):
-        for i in range(0,len(name)):
-            cursor.execute("INSERT INTO dbo.country(name,code,capital,region,subregion,population,area,lat,lon,timezone,currency,flag) values (?,?,?,?,?,?,?,?,?,?,?,?)", name[i],str(ccode[i]),capital[i],region[i],subregion[i],str(population[i]),str(area[i]),str(latlong[i][0]),str(latlong[i][1]),timezone[i],currency[i],flag[i])
-            connection.commit()
-
-    print(len(rows))
-    return jsonify(countries)
-    
