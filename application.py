354710ab8ac88a5f03110318b9325be1a28ee091
ARAVINDAKKSAN VIJAYAKUMAR <36226810+Aravindakksan@users.noreply.github.com>
2020-08-05 13:52:22 +0530

    Update application.py

--- application.py
+++ application.py
@@ -13,7 +13,9 @@ server = 'tcp:country.database.windows.net'
 database = 'rest country api' 
 username = 'aravind' 
 password = 'Akksan766764#'
-connection = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+drivers = [item for item in pyodbc.drivers()]
+driver = drivers[-1]
+connection = pyodbc.connect('DRIVER={driver};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM country")
 rows = cursor.fetchall()
